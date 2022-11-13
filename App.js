03fa5bbacf0c19e831a3bf77516fd0ed120f0fd2
Ananya <aaggrawal@ndsj.org>
2022-11-12 23:51:30 -0800

    made small corrections

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, Image, TouchableOpacity } from 'react-native';
 import { useEffect, useRef, useState } from 'react';
 import { Camera } from 'expo-camera';
 import { shareAsync } from 'expo-sharing';
 import callGoogleVisionAsync from "./helperFunctions";
+import MissingComponent from "./esha"
+import findFood from "./helperFindFood";
+import logo from "./assets/myreal1.png"
+
 
 export default function App() {
   let cameraRef = useRef();
   const [hasCameraPermission, setHasCameraPermission] = useState();
   const [photo, setPhoto] = useState();
-  const [goog, setGoog] = useState();
+  //const [goog, setGoog] = useState();
   const [photoduplicate, setPhotoduplicate] = useState();
   const [text, setText] = useState("Please add an image");
+  const [scannedvalues, setScannedvalues] = useState("Please add an image");
   useEffect(() => {
     (async () => {
       const cameraPermission = await Camera.requestCameraPermissionsAsync();
@@ -38,9 +43,10 @@ export default function App() {
     console.log("picture taken");
     const responseData = await callGoogleVisionAsync(newPhoto.base64);
     //await setTimeout (5000);
-    setGoog(responseData);
-    setText(responseData.description)
-    console.log(responseData);
+    //setGoog(responseData);
+    console.log("responseData ==> " , responseData);
+    setText(responseData.toString());
+    setScannedvalues(responseData);
     console.log("Called Google");
 
     //savePhoto();
@@ -56,19 +62,25 @@ export default function App() {
       });
     };
 
-
+    //scan = {text};
+    //scan = new Array(text) ; // what the image has from google api
+    let values = findFood(scannedvalues);
+    //console.log("Esha scan --> ", values[0]);
+    //console.log("Esha scan --> ", values[1]);
 
     return (
+      
       <SafeAreaView style={styles.container}>
-        <Text>{text}</Text>
-        <Text>{text}</Text>
-        <Text>{text}</Text>
-        <Text>{text}</Text>
-        <Text>{text}</Text>
+        <View style= {styles.textbox}>
+        <Text style={styles.presentText}>{values[0]} </Text>
+        <Text style={styles.missingText}>{values[1]}</Text>
+        </View>
+        {/* //<Text>{text}</Text> */}
         <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
-        <Button title="Share" onPress={sharePic} />
-        <Button title="Start Again" onPress={() => setPhoto(undefined)} />
-
+        <View style= {styles.textbox}>
+        <Button style = {styles.eshabutton} title="Share" onPress={sharePic} />
+        <Button tyle = {styles.eshabutton} title="Start Again" onPress={() => setPhoto(undefined)} />
+        </View>
       </SafeAreaView>
     );
   }
@@ -78,7 +90,12 @@ export default function App() {
   return (
     <Camera style={styles.container} ref={cameraRef}>
       <View style={styles.buttonContainer}>
-        <Button title="Take Food Pic" onPress={takePic} />
+        {/* <<Button title="Take Food Pic" onPress={takePic} />> */}
+        <TouchableOpacity style = {styles.buttonContainer} activeOpacity = {0.5} onPress={takePic}>
+          <Image source = {logo} style = {{width: 100, length: 50, resizeMode: 'center'}} ></Image>
+        </TouchableOpacity>
+
+
       </View>
       <StatusBar style="auto" />
     </Camera>
@@ -90,13 +107,66 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: "#3DA35D",
+    
+  },
+  textbox: {
+    borderWidth: 3,
+    borderRadius: 20,
+    width: 400,
+    backgroundColor:"#96E072",
+    borderColor: '#134611'
+    
   },
   buttonContainer: {
-    backgroundColor: '#fff',
-    alignSelf: 'flex-end'
+    //backgroundColor: '#fff',
+    alignSelf: 'flex-end',
+    margin: 10
   },
   preview: {
     alignSelf: 'stretch',
     flex: 1,
-  }
+    margin: 20,
+    borderWidth: 4,
+    borderRadius: 30,
+    borderColor: '#134611'
+  
+  },
+  eshabutton: {
+    backgroundColor: "khaki",
+    color: "hotpink", 
+    borderColor: '#134611'
+  },
+  baseText: {
+    fontFamily: 'serif',
+    padding: 50,
+    backgroundColor:"pink",
+    textAlign: 'center',
+    resizeMode: 'center',
+  },
+  titleText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: "hotpink", 
+    height: 150
+
+  },
+  presentText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: "green", 
+    height: 50,
+    alignSelf: "left",
+    paddingHorizontal: 20
+
+  },
+  missingText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: "indianred", 
+    height: 50,
+    alignSelf: "left",
+    paddingHorizontal: 20,
+
+  },
 });
